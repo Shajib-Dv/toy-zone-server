695f7a520bc7400a816195aaadf6d6fedf8bad45
Shajib-Dv <mdsojeeb242@gmail.com>
2023-05-18 22:23:27 +0600

    data load by category

--- index.js
+++ index.js
@@ -46,6 +46,14 @@ async function run() {
       res.send(result);
     });
 
+    //get data by category
+    app.get("/toys/category/:category", async (req, res) => {
+      const category = req.params.category;
+      const query = { subCategory: category };
+      const result = await toysCollection.find(query).toArray();
+      res.send(result);
+    });
+
     //get data by email
     app.get("/toys/:email", async (req, res) => {
       const email = req.params.email;
