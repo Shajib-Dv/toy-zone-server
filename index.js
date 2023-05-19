4cdcef8be235d2afc7d793cc0c37cc41a0657c11
Shajib-Dv <mdsojeeb242@gmail.com>
2023-05-19 19:26:26 +0600

    delete route added

--- index.js
+++ index.js
@@ -91,6 +91,14 @@ async function run() {
       res.send(result);
     });
 
+    //delete data by id
+    app.delete("/toys/toy/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toysCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.post("/toys", async (req, res) => {
       const toys = req.body;
       // console.log(toys);
